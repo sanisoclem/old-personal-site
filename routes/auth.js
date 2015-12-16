f4f4168fc513ebc416c25751101414f7748f5308
sanisoclem <me@jerahmeelcosinas.net>
2015-12-17 04:46:20 +0800

    oauth2 google login test

--- routes/auth.js
+++ routes/auth.js
@@ -1,28 +1,41 @@
 var url = require('url')
 var express = require('express');
-var GitkitClient = require('gitkitclient');
-var fs = require('fs');
+var google = require('googleapis');
 
-var gitkitClient = new GitkitClient(JSON.parse(fs.readFileSync('conf/google-server-config.json')));
 var router = express.Router();
+var OAuth2 = google.auth.OAuth2;
+var plus = google.plus('v1');
 
-router.all('/login', function(req, res, next) {
-    res.render('login', { postBody : encodeURIComponent(req.body || '') });
+router.post('/login', function(req, res, next) {
+  var oauth2Client = new OAuth2(process.env.googleClientId, process.env.googleClientSecret, url.resolve(process.env.siteUrl,'authenticated'));
+
+  res.redirect(oauth2Client.generateAuthUrl({
+    access_type: 'online', // 'online' (default) or 'offline' (gets refresh_token)
+    scope: ['https://www.googleapis.com/auth/plus.me',
+        'https://www.googleapis.com/auth/plus.login',
+        'https://www.googleapis.com/auth/userinfo.email',
+        'https://www.googleapis.com/auth/userinfo.profile']
+  }));
 });
 
-router.post('/sendemail', function (req, res) {
-    req.app.disable('etag');
-    gitkitClient.getOobResult(req.body, req.ip, req.cookies.gtoken, function(err, resp) {
-        if (err) {
-            console.log('Error: ' + JSON.stringify(err));
-        } else {
-            // Add code here to send email
-            console.log('Send email: ' + JSON.stringify(resp));
-        }
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        res.end(resp.responseBody);
-    })
+
+router.get('/authenticated', function(req, res, next) {
+    var oauth2Client = new OAuth2(process.env.googleClientId, process.env.googleClientSecret, url.resolve(process.env.siteUrl,'authenticated'));
+
+    oauth2Client.getToken(req.query.code, function(err, tokens) {
+        if (err)
+            res.redirect('/authfail');
+
+        oauth2Client.setCredentials(tokens);
+
+        plus.people.get({ userId: 'me', auth: oauth2Client }, function(err, response) {
+            res.render('authenticated', { response: err || JSON.stringify( {response: response,code: req.query.code,token:tokens} )});
+        });
+    });
+});
+
+router.get('/authfail', function(req, res, next) {
+    res.render('authfail', { });
 });
 
 
